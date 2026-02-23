6f5623e37a35e4f42443171d750915450d639305
19floresa <alexander.flores0920@gmail.com>
2026-02-22 16:13:53 -0800

    removed * cors

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin: '*',//'http://gui:3000',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST',],
     allowedHeaders: [ 'Access-Control-Allow-Origin','Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
