19ef65a5252d4900acbac9d038be5a1f576150fc
19floresa <alexander.flores0920@gmail.com>
2026-03-08 01:02:02 -0800

    testing new cors

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,13 +3,15 @@ import dotenv from "dotenv"
 dotenv.config()
 
 interface Config {
-    port: number;
-    nodeEnv: string;
+    port: number
+    nodeEnv: string
+    URL: string
 }
 
 const config: Config = {
     port: Number(process.env.PORT) || 3025,
     nodeEnv: process.env.NODE_ENV || "development",
+    URL: process.env.URL || "16.145.81.136" // temp solution
 }
 
 export default config
